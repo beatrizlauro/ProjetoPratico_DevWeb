507fe872649bac66c8e725422a6da14c917a5c7e
Beatriz Lauro <bia.ssparaiso@gmail.com>
2023-07-06 16:17:55 -0300

    (probably(or not)) att final

---
+++ contato/script.js
@@ -0,0 +1,8 @@
+function showModal0(){
+var element = document.getElementById("modal0");
+element.classList.add("show-modal0");
+}
+function hideModal0(){
+    var element = document.getElementById("modal0");
+    element.classList.remove("show-modal0");
+} 
